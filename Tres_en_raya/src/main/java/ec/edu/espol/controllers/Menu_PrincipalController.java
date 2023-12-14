816f8d91015089f5b54ea71e6e8c0db092213f23
Darvier88 <dpacheco@fiec.espol.edu.ec>
2023-12-13 23:30:32 -0600

    Actu

--- Tres_en_raya/src/main/java/ec/edu/espol/controllers/Menu_PrincipalController.java
+++ Tres_en_raya/src/main/java/ec/edu/espol/controllers/Menu_PrincipalController.java
@@ -4,12 +4,19 @@
  */
 package ec.edu.espol.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
@@ -26,12 +33,18 @@ public class Menu_PrincipalController implements Initializable {
     }    
     
     @FXML
-    public void jugar(MouseEvent event) {
+    public void jugar(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ec/edu/espol/tres_en_raya/Modos_de_juego.fxml"));
+        Parent Modos_de_juegoParent = loader.load();
+        Scene Modos_de_juegoScene = new Scene(Modos_de_juegoParent,680,480);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(Modos_de_juegoScene);
+        window.show();
     }
 
     @FXML
     public void exitApp(MouseEvent event) {
-    
+        Platform.exit();
     }
 
 }
---
+++ Tres_en_raya/src/main/java/ec/edu/espol/controllers/Modos_de_juegoController.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+/**
+ * FXML Controller class
+ *
+ * @author ASUS VIVOBOOK PRO
+ */
+public class Modos_de_juegoController implements Initializable {
+
+    @FXML
+    private ImageView tradicional;
+    private int indiceImg=0;
+    private String[] imagenesTrad={"tradicional1.png","tradicional2.png","tradicional3.png","tradicional4.png","tradicional5.png","tradicional6.png"};
+    private ScaleTransition scaleTransition;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }  
+
+
+
+    private void cambiarImgT(){
+        indiceImg=(indiceImg+1)%imagenesTrad.length;
+        String nombreImg = imagenesTrad[indiceImg];
+        Image img = new Image("ec/edu/espol/images/"+nombreImg);
+        tradicional.setImage(img);
+    }
+    private void restaurarOgT(){
+        indiceImg=0;
+        Image img = new Image("ec/edu/espol/images/"+imagenesTrad[indiceImg]);
+        tradicional.setImage(img);
+    }
+
+    @FXML
+    private void restaurarImg(MouseEvent event) {
+        scaleTransition.stop(); // Detener la transición actual si está en progreso
+        restaurarOgT();
+        tradicional.setScaleX(1); // Restablecer la escala en el eje X
+        tradicional.setScaleY(1); // Restablecer la escala en el eje Y
+    }
+
+    @FXML
+    private void agrandarImg(MouseEvent event) {
+        // Escalar la imagen
+        scaleTransition = new ScaleTransition(Duration.millis(300), tradicional);
+        scaleTransition.setToX(1.25);
+        scaleTransition.setToY(1.25);
+
+       
+        // Cambiar de imagen durante la transición de opacidad
+        scaleTransition.setOnFinished(e -> cambiarImagenYReproducirAnimacion(scaleTransition));
+
+
+        // Reproducir la animación
+        scaleTransition.play();
+    }
+
+    private void cambiarImagenYReproducirAnimacion(ScaleTransition scaleTransition) {
+        // Cambiar a la siguiente imagen
+        cambiarImgT();
+
+        // Reiniciar las transiciones
+        scaleTransition.setRate(0.75);
+
+        // Reproducir la animación nuevamente
+        scaleTransition.play();
+    }
+
+    @FXML
+    private void elegidoT(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ec/edu/espol/tres_en_raya/jugadoresT.fxml"));
+        Parent jugadoresTParent = loader.load();
+        Scene jugadoresTScene = new Scene(jugadoresTParent,680,480);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(jugadoresTScene);
+        window.show();
+    }
+    
+    
+}
