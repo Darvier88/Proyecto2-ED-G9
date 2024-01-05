73b52eceb0304600e4f06ca2b10347713591b7b4
2Dsa3 <davsum04@hotmail.com>
2024-01-04 23:14:45 -0500

    bian

---
+++ Tres_en_raya/src/main/java/ec/edu/espol/TDAs/TreeNode.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.TDAs;
+
+/**
+ *
+ * @author davsu
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TreeNode<E> {
+    private int[][] matriz;
+    private List<Tree> children;
+    private int utilidad_asociada;
+
+    public TreeNode(int[][] matriz) {
+        this.matriz = matriz;
+        this.children = new LinkedList<>();
+    }
+
+    public int[][] getContent() {
+        return this.matriz;
+    }
+
+    public void setContent(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public List<Tree> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Tree> children) {
+        this.children = children;
+    }
+
+    public Tree getTreeChildern(int i) {
+        return children.get(i);
+    }
+
+    public void addChildrenNode(Tree children) {
+        this.children.add(children);
+    }
+
+    
+
+}
