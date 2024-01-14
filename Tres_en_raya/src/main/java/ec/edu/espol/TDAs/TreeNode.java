a2810eb7a23b8361369cf47e80b8c1bb77846799
2Dsa3 <davsum04@hotmail.com>
2024-01-14 02:42:08 -0500

    gg

--- Tres_en_raya/src/main/java/ec/edu/espol/TDAs/TreeNode.java
+++ Tres_en_raya/src/main/java/ec/edu/espol/TDAs/TreeNode.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class TreeNode<E> {
     private int[][] matriz;
-    private List<Tree> children;
+    private LinkedList<Tree<E>> children;
     private int utilidad;
 
     public TreeNode(int[][] matriz) {
@@ -30,11 +30,11 @@ public class TreeNode<E> {
         this.matriz = matriz;
     }
 
-    public List<Tree> getChildren() {
+    public LinkedList<Tree<E>> getChildren() {
         return children;
     }
 
-    public void setChildren(List<Tree> children) {
+    public void setChildren(LinkedList<Tree<E>> children) {
         this.children = children;
     }
 
